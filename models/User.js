7e46d02d7833fab64e488ef2f640d87b7a6e4223
Christian Zenaty <cazenaty@yahoo.com>
2018-06-13 21:41:32 -0700

    Adding userPhotoURL property

--- models/User.js
+++ models/User.js
@@ -5,6 +5,7 @@ const userSchema = new Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   userName: { type: String, required: true },
+  userPhotoURL: { type: String, required: false },
   email: { type: String, required: true },
   location: {
     city: { type: String, required: true },
@@ -24,7 +25,7 @@ module.exports = User;
     * to create Documents (MongoDB term) to add to our Collection.  In this case a 'Collection of Users.'
     * Most of the userSchema should be rather self-explanatory,
     * but do note the isTechnical is how we will denote a "Technical Founder" if true vs 
-    * 'Non-Technical' if false.  
+    * 'Non-Technical' if false.  userPhotoURL will ideally be provided via the user's profile from OAuth.
     *
     * Also note, the 'connections' will be an array of Users that maps to each unique ObjectID that 
     * MongoDB automatically creates upon each new model/Schema that is created.
