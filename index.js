906174fe645bafc3d5c9685da366f2652444c5f8
Christian Zenaty <cazenaty@yahoo.com>
2018-06-19 11:45:43 -0700

    Refactoring of index routes

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const chalk = require("chalk");
 
+const indexRoutes = require("./routes/index");
+
 const app = express();
 
 /* Express Middleware */
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+/* Routes */
+app.use("/", indexRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
