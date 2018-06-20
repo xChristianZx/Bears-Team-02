7a904c591f685daaa29157e1c0cab0d62ae01e3e
Monica Babaian <monica.babaian@gmail.com>
2018-06-20 18:53:35 -0500

    Add footer

--- client/src/js/app.js
+++ client/src/js/app.js
@@ -1,10 +1,21 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import Home from "./Home/Home.js";
+import Header from "./Header/Header.js";
+import Footer from "./Footer/Footer.js";
 
 class App extends Component {
   render() {
-    return <div>Welcome to the world of React</div>;
+    return (
+      <div>
+        <Header />
+        <Home />
+        <Footer />
+      </div>
+      
+    );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<App/>, document.getElementById("app"));
+
