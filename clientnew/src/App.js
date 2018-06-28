bee650572913d380fbc8007bae9cda352bdb5859
Christian Zenaty <cazenaty@yahoo.com>
2018-06-27 17:05:33 -0700

    Initial upload and build

---
+++ clientnew/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./App.css";
+import Footer from "./components/Footer/Footer";
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ clientnew/src/components/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div>
+      <footer className="footer">
+        <div className="content has-text-centered">
+          <h3>
+            <strong>Founder Connect</strong> was created by a Chingu cohort.
+          </h3>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
