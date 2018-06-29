1c8f338f64fec7c992729ec81e6d53b0ed09f14c
Christian Zenaty <cazenaty@yahoo.com>
2018-06-29 14:36:37 -0700

    Adding React Router layout and reformat of Home page to Landing

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from "react";
 import "./App.css";
-import Home from "./components/Home/Home";
-import Header from "./components/Header/Header";
-// import Connections from "./components/Connections/Connections";
+import Connections from "./components/Connections/Connections";
+import Landing from "./components/Landing/Landing";
 import Footer from "./components/Footer/Footer";
+import { BrowserRouter, Route } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
-        <Home />
-        {/* <Connections /> */}
-        <Footer />
+        <BrowserRouter>
+          <Route exact path="/" component={Landing} />
+          <Route path="/connect" component={Connections} />
+          <Footer />
+        </BrowserRouter>
       </div>
     );
   }
