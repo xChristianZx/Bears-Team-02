3d2e87be18b3fb965a12258595f4011c10237b6a
AdamSackfield <=>
2018-07-03 21:57:25 +0100

    Added ReactRouter and history. Created basic navigation. HOC for aux, and history. Added signup route.

---
+++ client/src/containers/SignUp/SignUp.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+  class SignUp extends Component {
+    render() {
+      return (
+        <div>Sign Up Container</div>
+      )
+    }
+  }
+
+export default SignUp
