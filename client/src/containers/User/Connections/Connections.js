16d13646fd00ee968d474c0d059ffbd02f9856ea
Christian Zenaty <cazenaty@yahoo.com>
2018-07-19 12:49:39 -0700

    Updated styling and rendering of  connectionList

--- client/src/containers/User/Connections/Connections.js
+++ client/src/containers/User/Connections/Connections.js
@@ -16,16 +16,46 @@ class Connections extends Component {
     }
 
     const list = connections.map(connection => {
-      return <li key={connection._id}>Username: {connection.username}</li>;
+      return (
+        <li className="list-container" key={connection._id}>
+          <div className="media">
+            <div className="media-left">
+              <figure className="image is-96x96">
+                <img
+                  src="https://bulma.io/images/placeholders/96x96.png"
+                  alt="Placeholder"
+                />
+              </figure>
+            </div>
+            <div className="media-content">
+              <div className="media">
+                <div className="media-content">
+                  <p className="title is-4">
+                    {`${connection.firstName} ${connection.lastName}`}
+                  </p>
+                  <p className="subtitle is-6">
+                    Username: {connection.username}
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="media-right">
+              <button className="button is-primary">CONNECT</button>
+            </div>
+          </div>
+        </li>
+      );
     });
-    return <ul className="container">{list}</ul>;
+    return <ul className="">{list}</ul>;
   };
 
   render() {
     return (
-      <div className="container connections-container">
-        <h1 className="title">Connections</h1>
-        {this.renderList()}
+      <div className="container connections-container columns is-centered">
+        <div className="column is-three-quarters">
+          <h1 className="title has-text-centered">Connections</h1>
+          {this.renderList()}
+        </div>
       </div>
     );
   }
