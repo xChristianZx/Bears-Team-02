21b55ffb5e8a003de8725bd6c88307550fc57b82
Christian Zenaty <cazenaty@yahoo.com>
2018-07-18 23:19:25 -0700

    Initial refactor for connections

--- client/src/containers/User/Connections/Connections.js
+++ client/src/containers/User/Connections/Connections.js
@@ -1,44 +1,47 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
-import * as userActions from '../../../actions/UserActions'
+import React, { Component } from "react";
+import "./Connections.css";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import * as userActions from "../../../actions/UserActions";
 
-  class Connections extends Component {
-    componentDidMount() {
-      this.props.actions.getConnections()
-    }
-
-
-    render() {
-      let connections = null;
-      if(this.props.connections) {
-        connections = this.props.connections.map(connection => {
-          return (
-            <div key={connection._id}>
-              Username: {connection.username}
-            </div>
-          )
-        })
-      }
-
-      return (
-        <div>
-          <h1>Connections</h1>
-
-          {connections}
-        </div>
-      )
-    }
+class Connections extends Component {
+  componentDidMount() {
+    this.props.actions.getConnections();
   }
 
-  const mapStateToProps = (state) => {
-    return { connections: state.User.connections }
-  }
-
-  const mapDispatchToProps = (dispatch) => {
-    return {
-      actions: bindActionCreators(Object.assign(userActions), dispatch)
+  renderList = () => {
+    const { connections } = this.props;
+    if (connections === null || connections === undefined) {
+      return <div>Loading...</div>;
     }
-  }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Connections)
+    const list = connections.map(connection => {
+      return <li key={connection._id}>Username: {connection.username}</li>;
+    });
+    return <ul className="container">{list}</ul>;
+  };
+
+  render() {
+    return (
+      <div className="container connections-container">
+        <h1 className="title">Connections</h1>
+        {this.renderList()}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return { connections: state.User.connections };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    actions: bindActionCreators(Object.assign(userActions), dispatch)
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Connections);
