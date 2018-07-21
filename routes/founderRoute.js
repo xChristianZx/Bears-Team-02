ce17b5446fc2dcd8836c710e92743aa82e29dbff
Christian Zenaty <cazenaty@yahoo.com>
2018-07-21 12:18:11 -0700

    Add connect list filter

--- routes/founderRoute.js
+++ routes/founderRoute.js
@@ -1,15 +1,22 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
-const passport = require('passport');
+const passport = require("passport");
 
-const requireAuth = passport.authenticate('jwt', { session: false })
+const requireAuth = passport.authenticate("jwt", { session: false });
 
-router.get("/",requireAuth, (req, res) => {
+router.get("/", requireAuth, (req, res) => {
+  const loggedInUserID = req.user._id;
+  // * Need .toString for ObjectId comparison
   User.find({})
-    .then(userList => res.send(userList))
+    .populate("pendingConnectionRequests")
+    .then(userList => {
+      return userList.filter(
+        user => user._id.toString() !== loggedInUserID.toString()
+      );
+    })
+    .then(list => res.send(list))
     .catch(err => console.log(err));
-}); 
+});
 
 module.exports = router;
- 
