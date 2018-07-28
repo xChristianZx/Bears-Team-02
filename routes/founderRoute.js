2f59c278cfb750f30c6651d5dbecf01909fdf37a
Christian Zenaty <cazenaty@yahoo.com>
2018-07-28 12:43:00 -0700

    Changing loggedIn User filter for mongo operator

--- routes/founderRoute.js
+++ routes/founderRoute.js
@@ -5,16 +5,24 @@ const passport = require("passport");
 
 const requireAuth = passport.authenticate("jwt", { session: false });
 
+/* 
+  Main List Render - Aggregates list of connections that are not the current loggedInUser 
+and people the loggedInUser is currently not connected to 
+*/
+
+// Find all users
+// Filter out current loggedInUsers
+// Filter out current connections
 router.get("/", requireAuth, (req, res) => {
   const loggedInUserID = req.user._id;
   // * Need .toString for ObjectId comparison
-  User.find({})
+
+  // Filtering current loggedInUser ($ne === does not equal)
+  User.find({ _id: { $ne: loggedInUserID } })
+    //Required for filter on /client/connectcomp Btn Rendering
     .populate("pendingConnectionRequests")
-    .then(userList => {
-      return userList.filter(
-        user => user._id.toString() !== loggedInUserID.toString()
-      );
-    })
+    .populate("connections")
+    //Filter current connections
     .then(list => res.send(list))
     .catch(err => console.log(err));
 });
