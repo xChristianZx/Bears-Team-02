1d60c0bddbe6c596d34ba3bff486f2adb664d6eb
AdamSackfield <=>
2018-08-02 19:22:41 +0100

    General tweaks

--- client/src/components/UI/User/Dashboard/Connections/Connections.js
+++ client/src/components/UI/User/Dashboard/Connections/Connections.js
@@ -1,33 +1,31 @@
 import React from 'react';
 import './Connection.css';
-import List from '../../../../../misc/list';
 
 const Connections = ({ connections }) => {
 	let userConnections = connections.map(connection => {
 		const { _id, firstName, lastName, username, isTechnical } = connection	
 		return (
-			<List user={connection} />
-			// <li className="list-item-container" key={_id}>
-			// 	<div className="media">
-			// 		<div className="media-left">
-			// 			<figure className="image is-96x96">
-			// 				<img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder" />
-			// 			</figure>
-			// 		</div>
-			// 		<div className="media-content">
-			// 			<div className="media">
-			// 				<div className="media-content">
-			// 					<p className="title is-4">{`${firstName} ${lastName}`}</p>
-			// 					<p className="subtitle is-6">@{username}</p>
-      //           <p disabled className="subtitle is-6">{isTechnical ? "Technical" : "Non-Technical"}</p>
-      //           <button className="button is-primary is-outlined">Message</button>
-      //           <button className="button is-danger is-outlined">Remove Connection</button>
-			// 					{/* <p className="subtitle is-6">ID: (for testing): {_id}</p> */}
-			// 				</div> 
-			// 			</div>
-			// 		</div>
-			// 	</div>
-			// </li>
+			<li className="list-item-container" key={_id}>
+				<div className="media">
+					<div className="media-left">
+						<figure className="image is-96x96">
+							<img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder" />
+						</figure>
+					</div>
+					<div className="media-content">
+						<div className="media">
+							<div className="media-content">
+								<p className="title is-4">{`${firstName} ${lastName}`}</p>
+								<p className="subtitle is-6">@{username}</p>
+                <p disabled className="subtitle is-6">{isTechnical ? "Technical" : "Non-Technical"}</p>
+                <button className="button is-primary is-outlined">Message</button>
+                <button className="button is-danger is-outlined">Remove Connection</button>
+								{/* <p className="subtitle is-6">ID: (for testing): {_id}</p> */}
+							</div> 
+						</div>
+					</div>
+				</div>
+			</li>
 		);
 	});
 	return (
