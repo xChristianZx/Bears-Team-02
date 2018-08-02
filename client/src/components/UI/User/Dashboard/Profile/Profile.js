3719d897ed52f626997af87f7cff332382de1539
Christian Zenaty <cazenaty@yahoo.com>
2018-08-02 13:22:49 -0700

    Fixing className log error

--- client/src/components/UI/User/Dashboard/Profile/Profile.js
+++ client/src/components/UI/User/Dashboard/Profile/Profile.js
@@ -5,24 +5,24 @@ const Profile = ({ user }) => {
 	const { firstName, lastName, username, email } = user;
 	return (
 		<div className="container connect-container is-centered">
-			<div class="card">
+			<div className="card">
 				<h1>Profile</h1>
-				<div class="card-content">
-					<div class="media">
-						<div class="media-left">
-							<figure class="image is-48x48">
+				<div className="card-content">
+					<div className="media">
+						<div className="media-left">
+							<figure className="image is-48x48">
 								<img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image" />
 							</figure>
 						</div>
-						<div class="media-content">
-							<p class="title is-4">
+						<div className="media-content">
+							<p className="title is-4">
 								{firstName} {lastName}
 							</p>
-							<p class="subtitle is-6">@{username}</p>
+							<p className="subtitle is-6">@{username}</p>
 						</div>
 					</div>
 
-					<div class="content">Write your bio here someday.</div>
+					<div className="content">Write your bio here someday.</div>
 				</div>
 			</div>
 		</div>
