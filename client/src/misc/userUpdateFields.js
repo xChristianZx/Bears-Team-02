4fe25f52f6c186afe2e8ceafd2f7cbac11428d3e
Christian Zenaty <cazenaty@yahoo.com>
2018-08-07 16:09:12 -0700

    [WIP] update user fields

---
+++ client/src/misc/userUpdateFields.js
@@ -0,0 +1,11 @@
+const userUpdateFields = [
+    { label: 'First Name', name: 'firstName', type: 'text', errorMsg: 'First Name is required' },
+    { label: 'Last Name', name: 'lastName', type: 'text', errorMsg: 'Last Name is required' },
+    { label: 'User Name', name: 'username', type: 'text', errorMsg: 'User Name is required' },
+    { label: 'Email', name: 'email', type: 'email', errorMsg: 'Email is required' },
+    { label: 'Old Password', name: 'password', type: 'password', errorMsg: 'Old Password is required' },
+    { label: 'New Password', name: 'password', type: 'password', errorMsg: 'New Password is required' },
+    { label: 'Confirm New Password', name: 'confirmPassword', type: 'password', errorMsg: 'Passwords do not match' }
+  ]
+   
+  export default userUpdateFields;
