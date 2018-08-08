e59bcbd6911de9f6e0a12c0ed5b5a7d8582d5b68
Christian Zenaty <cazenaty@yahoo.com>
2018-08-08 11:15:25 -0700

    Removing Update Password fields for now, will handle separately

--- client/src/misc/userUpdateFields.js
+++ client/src/misc/userUpdateFields.js
@@ -3,9 +3,9 @@ const userUpdateFields = [
     { label: 'Last Name', name: 'lastName', type: 'text', errorMsg: 'Last Name is required' },
     { label: 'User Name', name: 'username', type: 'text', errorMsg: 'User Name is required' },
     { label: 'Email', name: 'email', type: 'email', errorMsg: 'Email is required' },
-    { label: 'Old Password', name: 'password', type: 'password', errorMsg: 'Old Password is required' },
-    { label: 'New Password', name: 'password', type: 'password', errorMsg: 'New Password is required' },
-    { label: 'Confirm New Password', name: 'confirmPassword', type: 'password', errorMsg: 'Passwords do not match' }
+    // { label: 'Old Password', name: 'oldPassword', type: 'password', errorMsg: 'Old Password is required' },
+    // { label: 'New Password', name: 'newPassword', type: 'password', errorMsg: 'New Password is required' },
+    // { label: 'Confirm New Password', name: 'confirmNewPassword', type: 'password', errorMsg: 'Passwords do not match' }
   ]
    
   export default userUpdateFields;
