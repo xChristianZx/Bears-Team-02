7a06a63ce209c08296041d1d67e59ed9ffb373a3
Christian Zenaty <cazenaty@yahoo.com>
2018-08-08 14:04:47 -0700

    [WIP] Adding handler for isTechnical radio button

--- client/src/components/UI/Form/formFieldRender.js
+++ client/src/components/UI/Form/formFieldRender.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React from "react";
 
-const formFieldRender = ({ input, label, type, meta: { touched, error } }) => (
-	<div className="field">
-		<label className="label">{label}</label>
-		<div className="control">
-			<input className="input" type={type} {...input} />
-		</div>
-		<p className="help is-danger">{touched && (error && <span>{error}</span>)}</p>
-	</div>
-);
+const formFieldRender = ({ input, label, type, meta: { touched, error } }) => {
+// TODO - Handle Technical/ Non-technical
+// * Redux form currently does not recognize the boolean fields for isTechnical; may have to turn into string
+	//   console.log("INPUT", input)
+// 	if (type === "radio") {
+//     return (
+//       <div className="field">
+// 	  <label className="label" >{label}</label>
+//         <div className="control">
+//           <label className="radio">
+//             <input type={type} name="isTechnical" defaultChecked={input.value}/> Yes
+//           </label>
+//           <label className="radio">
+//             <input type={type} name="isTechnical" defaultChecked={input.value}/> No
+//           </label>
+//         </div>
+//       </div>
+//     );
+//   }
+  return (
+    <div className="field">
+      <label className="label">{label}</label>
+      <div className="control">
+        <input className="input" type={type} {...input} />
+      </div>
+      <p className="help is-danger">
+        {touched && (error && <span>{error}</span>)}
+      </p>
+    </div>
+  );
+};
 
-export default formFieldRender
+export default formFieldRender;
