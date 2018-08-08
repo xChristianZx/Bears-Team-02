52dcbe2ee5155a9e83c6a14d0fad6aff363c4afa
Christian Zenaty <cazenaty@yahoo.com>
2018-08-08 14:09:57 -0700

    [WIP] Adding isTechnical for future update handling

--- client/src/misc/userUpdateFields.js
+++ client/src/misc/userUpdateFields.js
@@ -1,11 +1,12 @@
 const userUpdateFields = [
-    { label: 'First Name', name: 'firstName', type: 'text', errorMsg: 'First Name is required' },
-    { label: 'Last Name', name: 'lastName', type: 'text', errorMsg: 'Last Name is required' },
-    { label: 'User Name', name: 'username', type: 'text', errorMsg: 'User Name is required' },
-    { label: 'Email', name: 'email', type: 'email', errorMsg: 'Email is required' },
-    // { label: 'Old Password', name: 'oldPassword', type: 'password', errorMsg: 'Old Password is required' },
-    // { label: 'New Password', name: 'newPassword', type: 'password', errorMsg: 'New Password is required' },
-    // { label: 'Confirm New Password', name: 'confirmNewPassword', type: 'password', errorMsg: 'Passwords do not match' }
-  ]
-   
-  export default userUpdateFields;
+  { label: 'First Name', name: 'firstName', type: 'text', errorMsg: 'First Name is required' },
+  { label: 'Last Name', name: 'lastName', type: 'text', errorMsg: 'Last Name is required' },
+  { label: 'User Name', name: 'username', type: 'text', errorMsg: 'User Name is required' },
+  { label: 'Email', name: 'email', type: 'email', errorMsg: 'Email is required' },
+  // { label: 'Are you a Technical Founder?', name: 'isTechnical', type: "radio" }
+  // { label: 'Old Password', name: 'oldPassword', type: 'password', errorMsg: 'Old Password is required' },
+  // { label: 'New Password', name: 'newPassword', type: 'password', errorMsg: 'New Password is required' },
+  // { label: 'Confirm New Password', name: 'confirmNewPassword', type: 'password', errorMsg: 'Passwords do not match' }
+]
+
+export default userUpdateFields;
