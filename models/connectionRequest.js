56ad317a51b9ce7448f19c3f686021827057b48c
Christian Zenaty <cazenaty@yahoo.com>
2018-08-09 11:48:07 -0700

    Moving /connections routes to appropriate folder

--- models/connectionRequest.js
+++ models/connectionRequest.js
@@ -4,11 +4,11 @@ const { Schema } = mongoose;
 const ConnectionRequestSchema = new Schema({
   requestingUser: { type: Schema.Types.ObjectId, ref: "User", index: true }, // User that is making the connection request
   requestedUser: { type: Schema.Types.ObjectId, ref: "User", index: true }, // User that is being requested to connect
-  status: { type: String, default: 'Pending' } // Pending, Accepted, Rejected
+  status: { type: String, default: "Pending" } // Pending, Accepted, Rejected
 });
 
-const ConnectionRequest = mongoose.model('ConnectionRequest', ConnectionRequestSchema)
- 
+const ConnectionRequest = mongoose.model("ConnectionRequest", ConnectionRequestSchema);
+
 module.exports.create = (newConnectionRequest, callback) => {
-  newConnectionRequest.save(callback)
-}
+  newConnectionRequest.save(callback);
+};
--- routes/connectionRoutes.js
+++ routes/connectionRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/User");
+const mongoose = require("mongoose");
 const passport = require("passport");
+const User = require("../models/User");
+const ConnectionRequest = mongoose.model("ConnectionRequest");
 const requireAuth = passport.authenticate("jwt", { session: false });
 
-//  * Route prefix: /connection 
-
+/* Route prefix: /connections */
 
 // == Main Handler for /connect list render == //
 router.get("/", requireAuth, (req, res) => {
@@ -27,9 +28,7 @@ router.get("/", requireAuth, (req, res) => {
     //Filter isTech/Non-Tech - if none(undefined) === all founders
     .then(userList => {
       if (isTechnical === "false" || isTechnical === "true") {
-        return userList.filter(
-          user => user.isTechnical.toString() === isTechnical
-        );
+        return userList.filter(user => user.isTechnical.toString() === isTechnical);
       } else {
         return userList;
       }
@@ -38,9 +37,171 @@ router.get("/", requireAuth, (req, res) => {
     .catch(err => console.log(err));
 });
 
+/*  Endpoint for getPendingConnections() Action Creator*/
+router.get("/pendingconnections", requireAuth, (req, res) => {
+  let connectionRequests = {
+    pending: [],
+    acceptable: []
+  };
+
+  ConnectionRequest.find({ requestingUser: req.user._id })
+    .populate("requestedUser")
+    .exec((err, connReqs) => {
+      let error = null;
+
+      if (err) {
+        error = err;
+      }
+
+      connReqs.map(connReq => {
+        if (connReq.status === "Pending") {
+          connectionRequests.pending.push(connReq);
+        }
+      });
+
+      ConnectionRequest.find({ requestedUser: req.user._id })
+        .populate("requestingUser")
+        .exec((err, connReqs) => {
+          if (err) {
+            error = err;
+          }
+          connReqs.map(connReq => {
+            if (connReq.status === "Pending") {
+              connectionRequests.acceptable.push(connReq);
+            }
+          });
+
+          let count = connectionRequests.pending.length + connectionRequests.acceptable.length;
+          if (error) {
+            res.json({
+              success: false,
+              error
+            });
+          } else {
+            res.json({
+              success: true,
+              connectionRequests,
+              pendingRequests: count
+            });
+          }
+        });
+    });
+});
+
+/* Send Connection Request */
+router.post("/connectionrequest", requireAuth, (req, res) => {
+  let newConnectionRequest = {
+    requestingUser: req.user._id,
+    requestedUser: req.body.requestedUser
+  };
+  // console.log(`NOTE to self: newConnectionRequest .create is commented out; not creating new requests`)
+  ConnectionRequest.create(newConnectionRequest, (err, conReq) => {
+    if (conReq) {
+      res.json({
+        success: true,
+        conReq
+      });
+    }
+  });
+});
+
+/*
+	Takes a connection request ID the requesting user and action. It then finds the connection
+	changes the status to the given action. If the action is accepted it will add the new
+	connection to each users connections array.
+*/
+router.post("/pendingconnectionresponse", requireAuth, (req, res) => {
+  let connectionRequest = req.body.connectionRequest.toString();
+  let acceptingUser = req.user._id.toString();
+  let action = req.body.action;
+
+  ConnectionRequest.findById(connectionRequest, (err, connReq) => {
+    if (err) {
+      res.json({
+        success: false,
+        error: err
+      });
+    }
+
+    connReq.status = action;
+    connReq.save();
+
+    if (action === "Accepted") {
+      User.findById(acceptingUser, (err, user) => {
+        if (err) {
+          res.json({
+            success: false,
+            error: err
+          });
+        }
+        user.connections.push(connReq.requestingUser);
+        user.save();
+
+        User.findById(connReq.requestingUser, (err, user) => {
+          if (err) {
+            res.json({
+              success: false,
+              error: err
+            });
+          }
+
+          user.connections.push(acceptingUser);
+          user.save();
+          res.json({
+            success: true,
+            message: "Connection request accepted."
+          });
+        });
+      });
+    } else {
+      res.json({
+        success: true,
+        message: "Connection request declined."
+      });
+    }
+  });
+});
+
+/* Block/ Hide a connection */
+router.post("/blockconnection", requireAuth, (req, res) => {
+  const loggedInUserId = req.user._id;
+  const userToBlock = req.body.blockedUserId;
+
+  // ? - Better way to handle multiple docs update async; as queries do not return promises
+
+  /*  1. Find userToBlock and remove loggedInUser from their connections list */
+  const updateBlockedUserConnections = User.findByIdAndUpdate(
+    userToBlock,
+    {
+      $addToSet: { hiddenUsers: loggedInUserId },
+      $pull: { connections: loggedInUserId }
+    },
+    { new: true }
+  );
+
+  /*  2. Find loggedInUser and remove userToBlock from connections and add to blockedConnections */
+  const updateLoggedInUser = User.findByIdAndUpdate(
+    loggedInUserId,
+    {
+      $addToSet: { blockedConnections: userToBlock },
+      $pull: { connections: userToBlock }
+    },
+    { new: true }
+  );
+
+  return Promise.all([updateBlockedUserConnections, updateLoggedInUser])
+    .then(() =>
+      User.findById(loggedInUserId)
+        .populate("connections")
+        //   .populate("blockedConnections")  // No need to populate for now, id should suffice
+        .then(user => res.status(200).send({ message: "Connection Blocked", user }))
+    )
+    .catch(error => res.status(400).send({ error: error.message }));
+});
+
 module.exports = router;
 
-/* Note to future self */
+/* Note to future self re: block connection*/
 
 // Started here
 /* User.find({
