b668807bb321e559ed692f6d76ed4d2da66e79d1
Christian Zenaty <cazenaty@yahoo.com>
2018-08-14 08:52:04 -0700

    Iterator fix for messages

--- client/src/components/UI/User/Dashboard/Messages/Conversation.js
+++ client/src/components/UI/User/Dashboard/Messages/Conversation.js
@@ -4,9 +4,10 @@ import ConversationReply from '../../../../../containers/Message/ConversationRep
 import './Conversation.css';
 
 const Conversation = ({ conversationId, subject, messages, user, markAsRead }) => {
-	let { _id, firstName, lastName } = user;
-	let messagesList = messages.map(message => {
-		return <Message user={user} message={message} firstName={firstName} markAsRead={markAsRead} />;
+	const { _id, firstName, lastName } = user;
+	console.log("HERE", messages);
+	const messagesList = messages.map(message => {
+		return <Message key={message.dateSent} user={user} message={message} firstName={firstName} markAsRead={markAsRead} />;
 	});
 	return (
 		<React.Fragment>
