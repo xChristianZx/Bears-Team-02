caf433ea59620c619007826e366544b04b7e5d89
Christian Zenaty <cazenaty@yahoo.com>
2018-08-13 20:46:49 -0700

    Unused Imports cleanup

--- client/src/components/UI/Enhancements/Capitalize.js
+++ client/src/components/UI/Enhancements/Capitalize.js
@@ -1,7 +1,5 @@
-import React from 'react'
-
 const Capitalize = string => {
   return string.charAt(0).toUpperCase() + string.slice(1);
-}
+};
 
-export default Capitalize
+export default Capitalize;
--- client/src/components/UI/User/Dashboard/Messages/Message.js
+++ client/src/components/UI/User/Dashboard/Messages/Message.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import './Message.css';
 
 // const Message = ({ message, user, markAsRead }) => {
