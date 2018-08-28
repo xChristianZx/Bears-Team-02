0e16a3a2fb9b95fd16e87d25112472d8a0ea515c
Christian Zenaty <cazenaty@yahoo.com>
2018-08-28 16:35:16 -0700

    Updating Footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -3,10 +3,13 @@ import "./Footer.css";
 
 const Footer = () => {
   return (
-    <footer className="footer">
+    <footer className="footer has-background-grey-lighter">
       <div className="content has-text-centered">
         <p className="subtitle is-6">
-          Founder Connect was created by a Chingu cohort.
+          Created by a{" "}
+          <a href="https://github.com/chingu-voyage6/Bears-Team-02" target="_blank" rel="noopener noreferrer">
+            Chingu cohort
+          </a>
         </p>
       </div>
     </footer>
